055cb6f600495947e409fa30561ca0d276e4d2d6
Igor Muravev <igor.muravev@simbirsoft.com>
2016-08-11 16:47:55 +0400

    initialize editor via constructor

---
+++ src/config.js
@@ -0,0 +1,313 @@
+(function($){
+    var config = {};
+    config.entities = [];
+    var staff = {
+        "name": "staff",
+        "label": "Сотрудники",
+        "backend": {
+            "url": "http://universal-backend.dev/rest/v1/staff",
+            "sortBy": "-id",
+            "fields": {
+                "primaryKey": "id",
+                "parent": "parent_id"
+            }
+        },
+        "tabs": [
+            {
+                "label": "Карточка",
+                "fields": [
+                    {
+                        "name": "name",
+                        "type": "string",
+                        "list": true,
+                        "label": "Имя",
+                        "required": true
+                    },
+                    {
+                        "name": "email",
+                        "type": "string",
+                        "list": true,
+                        "label": "Эл. почта",
+                        "required": true
+                    },
+                    {
+                        "name": "gender",
+                        "type": "radiolist",
+                        "list": true,
+                        "label": "Пол",
+                        "filterable": true,
+                        "values": {
+                            "male" : "Мужской",
+                            "female" : "Женский"
+                        },
+                        "showOnly" : "edit"
+                    },
+                    {
+                        "name": "country",
+                        "type": "select",
+                        "list": true,
+                        "label": "Страна",
+                        "filterable": true,
+                        "valuesRemote": {
+                            "fields": {
+                                "value": "id",
+                                "label": "name"
+                            },
+                            "url": "http://universal-backend.dev/rest/v1/country"
+                        }
+                    },
+                    {
+                        "name": "notes",
+                        "label": "Заметки",
+                        "type": "array",
+                        "fields": [
+                            {
+                                "name": "color",
+                                "type": "colorpicker",
+                                "list": true,
+                                "label": "Любимый цвет"
+                            },
+                            {
+                                "name": "text",
+                                "type": "textarea",
+                                "label": "Дополнительные заметки"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "label": "Служебное",
+                "fields": [
+                    {
+                        "name": "fired",
+                        "type": "checkbox",
+                        "label": "Уволен",
+                        "values": {
+                            "1": ""
+                        }
+                    },
+                    {
+                        "name": "created_at",
+                        "type": "datetime",
+                        "readonly": true,
+                        "label": "Дата создания"
+                    },
+                    {
+                        "name": "updated_at",
+                        "type": "datetime",
+                        "readonly": true,
+                        "label": "Дата обновления"
+                    }
+                ]
+            }
+        ],
+        "contextMenu": [
+            {
+                "label": "Раскрыть",
+                "type": "open"
+            },
+            {
+                "label": "Редактировать",
+                "type": "edit"
+            },
+            {
+                "label": "Удалить",
+                "type": "delete"
+            }
+        ],
+        "actions": [
+            {
+                "type": "create",
+                "label": "Создать"
+            },
+            {
+                "type": "add",
+                "label": "Добавить"
+            },
+            {
+                "type": "update",
+                "label": "Обновить"
+            },
+            {
+                "type": "delete",
+                "label": "Удалить"
+            }
+        ],
+        "listHeaderBar": [
+            {
+                "type": "create",
+                "label": "Создать"
+            }
+        ],
+        "editFooterBar": [
+            {
+                "type": "update",
+                "label": "Сохранить"
+            },
+            {
+                "type": "presave",
+                "label": "Применить"
+            },
+            {
+                "type": "delete",
+                "label": "Удалить"
+            }
+        ]
+    };
+    var news = {
+        "name": "news",
+        "label": "Новости",
+        "backend": {
+            "url": "http://universal-backend.dev/rest/v1/news",
+            "sortBy": "-id",
+            "fields": {
+                "primaryKey": "id",
+                "parent": "parent_id"
+            }
+        },
+        "tabs": [
+            {
+                "label": "Новость",
+                "fields": [
+                    {
+                        "name": "published",
+                        "type": "checkbox",
+                        "list": true,
+                        "label": "Опубликовано",
+                        "values": {
+                            "1": ""
+                        }
+                    },
+                    {
+                        "name": "published_at",
+                        "type": "datetime",
+                        "list": true,
+                        "label": "Дата публикации"
+                    },
+                    {
+                        "name": "category_id",
+                        "type": "select",
+                        "label": "Категория",
+                        "list": true,
+                        "filterable": true,
+                        "valuesRemote": {
+                            "fields": {
+                                "value": "id",
+                                "label": "title"
+                            },
+                            "url": "http://universal-backend.dev/rest/v1/news/category"
+                        },
+                        "search": true,
+                        "tree": {
+                            "parentField": "parent_id",
+                            "childCountField": "child_count",
+                            "selectBranches": true
+                        }
+                    },
+                    {
+                        "name": "title",
+                        "type": "string",
+                        "list": true,
+                        "label": "Заголовок",
+                        "required": true
+                    },
+                    {
+                        "name": "description",
+                        "type": "textarea",
+                        "list": true,
+                        "label": "Краткое описание",
+                        "required": true
+                    },
+                    {
+                        "name": "text",
+                        "type": "wysiwyg",
+                        "label": "Текст"
+                    }
+                ]
+            },
+            {
+                "label": "Место",
+                "fields": [
+                    {
+                        "name": "coordinates",
+                        "type": "map",
+                        "label": "Местоположение события"
+                    }
+                ]
+            },
+            {
+                "label": "Служебное",
+                "fields": [
+                    {
+                        "name": "created_at",
+                        "type": "datetime",
+                        "readonly": true,
+                        "label": "Дата создания"
+                    },
+                    {
+                        "name": "updated_at",
+                        "type": "datetime",
+                        "readonly": true,
+                        "label": "Дата обновления"
+                    }
+                ]
+            }
+        ],
+        "contextMenu": [
+            {
+                "label": "Раскрыть",
+                "type": "open"
+            },
+            {
+                "label": "Редактировать",
+                "type": "edit"
+            },
+            {
+                "label": "Удалить",
+                "type": "delete"
+            }
+        ],
+        "actions": [
+            {
+                "type": "create",
+                "label": "Создать"
+            },
+            {
+                "type": "add",
+                "label": "Добавить"
+            },
+            {
+                "type": "update",
+                "label": "Обновить"
+            },
+            {
+                "type": "delete",
+                "label": "Удалить"
+            }
+        ],
+        "listHeaderBar": [
+            {
+                "type": "create",
+                "label": "Создать"
+            }
+        ],
+        "editFooterBar": [
+            {
+                "type": "update",
+                "label": "Сохранить"
+            },
+            {
+                "type": "presave",
+                "label": "Применить"
+            },
+            {
+                "type": "delete",
+                "label": "Удалить"
+            }
+        ]
+    };
+    config.entities.push(staff);
+    config.entities.push(news);
+    var editor = new UniversalEditor('universal-editor', config);
+})(jQuery);
--- src/module/universal-editor.configFile.js
+++ src/module/universal-editor.configFile.js
@@ -1,28 +1,10 @@
-(function($){
+function UniversalEditor(id, config) {
     'use strict';
 
     var app = angular.module('universal.editor');
-
-    $(document).ready(function(){
-        var http = angular.injector(['ng']).get('$http');
-        var unEditor = $('#universal-editor');
-        var url = unEditor[0].getAttribute('data-config-url');
-        unEditor.append("<div data-ui-view=''></div>");
-        if (url !== null){
-            http({
-                method : "GET",
-                url : url
-            }).then(function (response) {
-                app.constant('configData', response.data);
-                angular.bootstrap(unEditor[0], ["unEditor"]);
-                console.info("Config file successfuly loaded from API. Using remote config file from: "+ url);
-            }, function (reject) {
-                angular.bootstrap(unEditor[0], ["unEditor"]);
-                console.warn("Config file load error. Using local config file data.");
-            });
-        } else {
-            angular.bootstrap(unEditor[0], ["unEditor"]);
-            console.warn("Config file url not presented. Using local config file data.");
-        }
-    });
-})(jQuery);
+    var configData = config;
+    var unEditor = $('#' + id);
+    unEditor.append("<div data-ui-view=''></div>");
+    app.constant('configData', config);
+    angular.bootstrap(unEditor[0], ["unEditor"]);
+}
