4c03c60812dfbb5f0712d1c24111bd529b40d071
Igor Muravev <igor.muravev@simbirsoft.com>
2016-08-25 11:22:56 +0400

    fix bug loading list

--- src/module/directives/universalEditor/universalEditorList.tpl.js
+++ src/module/directives/universalEditor/universalEditorList.tpl.js
@@ -8,7 +8,7 @@ module.run(['$templateCache', function($templateCache) {
   $templateCache.put('module/directives/universalEditor/universalEditorList.html',
     '\n' +
     '<ul data-ng-if="vm.configData.entities.length &gt; 1" class="nav nav-tabs">\n' +
-    '    <li data-ng-repeat="entityItem in vm.configData.entities track by $index" data-ng-class="(entityItem.name === entity) ? \'active\' : \'\'" class="item"><a href="#/editor/{{entityItem.name}}/list">{{entityItem.label}}</a></li>\n' +
+    '    <li data-ng-repeat="entityItem in vm.configData.entities track by $index" data-ng-class="(entityItem.name === entity) ? \'active\' : \'\'" class="item"><a data-ui-sref="editor.type.list({type: entityItem.name})" ui-sref-opts="{reload: true, inherit: false}">{{entityItem.label}}</a></li>\n' +
     '</ul>\n' +
     '<div class="universal-editor">\n' +
     '    <div>\n' +
