46041ef827be6e46179126d7993fd18c212c0551
alexei.sorokin <alexei.sorokin@simbirsoft.com>
2016-12-29 17:12:14 +0400

    webpack config

--- src/module/universal-editor.js
+++ src/module/universal-editor.js
@@ -4,17 +4,30 @@
  * @param {Object} config - Configuration object
  * @constructor
  */
-function UniversalEditor(id, config) {
-    'use strict';
-    var moduleName = 'unEditor-' + id;
-    var unEditor = $('#' + id);
-    if (unEditor[0]) {
-        var app = angular.module(moduleName, ['universal.editor', 'ui.bootstrap']).config(function($stateProvider, $urlRouterProvider, $injector, configDataProvider) {
-            configDataProvider.setConfig(id, config); 
-        });
+require("angular");
+define("universal-editor", ["require"], function(require) {
+    return function(id, config) {
+        'use strict';
+        var moduleName = 'unEditor-' + id;
+        var unEditor = $('#' + id);
+        if (unEditor[0]) {
+            var app = angular.module(moduleName, ['universal.editor', 'ui.bootstrap']).config(function($stateProvider, $urlRouterProvider, $injector, configDataProvider) {
+                configDataProvider.setConfig(id, config);
+            });
+            unEditor.append("<div class='u-editors' data-ui-view='" + id + "'></div>");
+            angular.bootstrap(unEditor[0], [moduleName]);
+        }
+    };
+});
 
-        unEditor.append("<div class='u-editors' data-ui-view='" + id + "'></div>");
-        angular.bootstrap(unEditor[0], [moduleName]);
-    }
-}
+
+require('./universal-editor.module.js');
+require('./localization.configFile.js');
+var context = require.context('./components', true, /\.js$/);
+    context.keys().forEach(context);
+/*['./components', './controllers', './directives', './factories', './provider', './services'].forEach(function(dir) {
+    var context = require.context(dir, true, /\.js$/);
+    context.keys().forEach(context);
+});*/
+
 
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var BowerWebpackPlugin = require("bower-webpack-plugin");
+
+var webpackConfig = {
+    context: __dirname,
+    entry: {
+        'u-editor': './src/module/universal-editor.js',
+        styles: './src/index.scss'
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].core.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('u-editor.all.css', {
+            allChunks: true
+        }),
+        new BowerWebpackPlugin({
+            modulesDirectories: ['bower_components'],
+            manifestFiles: ['bower.json', '.bower.json'],
+            excludes: []
+        }),
+        new CopyWebpackPlugin([{
+            from: __dirname + '/src/index.html'
+        }]),
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            inject: 'body'
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+
+        /*,
+        new webpack.ProvidePlugin({
+            $: './bower_components/jquery/dist/jquery.js'
+        })*/
+        //new webpack.optimize.CommonsChunkPlugin('inline', 'inline.js'),
+        // new webpack.optimize.CommonsChunkPlugin({ name: 'bundle', filename: 'u-editor.all.js', minChunks: 2, chunks: ['vendor', 'u-editor'] })
+        // new webpack.optimize.CommonsChunkPlugin({ name: 'common', filename: 'u-editor.all.js', minChunks: 2, chunks: ['main', 'vendor'] })
+    ]
+};
+module.exports = webpackConfig;
+
