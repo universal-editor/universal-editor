c0f7acd386b268b94864469c40bf7e85b8f9fc5d
muravev <muravevigorek@gmail.com>
2017-01-13 15:07:00 +0400

    add modTemplates filter

--- src/module/components/ueRadiolist/ueRadiolist.tpl.js
+++ src/module/components/ueRadiolist/ueRadiolist.tpl.js
@@ -8,8 +8,8 @@ module.run(['$templateCache', function($templateCache) {
   $templateCache.put('module/components/ueRadiolist/ueRadiolist.html',
     '\n' +
     '<div ng-class="{\'field-wrapper row\':!vm.options.filter, \'filter-wrapper-field\': vm.options.filter}">\n' +
-    '    <div on-render-template="on-render-template" ng-class="{\'component-filter\': vm.options.filter,                   \'component-edit\': ((vm.templates.edit &amp;&amp; !vm.options.filter) || (vm.templates.filter &amp;&amp; vm.options.filter)) &amp;&amp; vm.regim === \'edit\',                   \'component-preview\': vm.templates.preview &amp;&amp; vm.regim === \'preview\'}" class="component-template"></div>\n' +
-    '    <div ng-if="((!vm.templates.edit &amp;&amp; !vm.options.filter) || (!vm.templates.filter &amp;&amp; vm.options.filter)) &amp;&amp; vm.regim === \'edit\'" ng-class="{\'component-filter\': vm.options.filter}" class="component-edit"> \n' +
+    '    <div on-render-template="on-render-template" ng-class="{\'component-filter\': vm.templates.filter &amp;&amp; vm.regim === \'filter\',                   \'component-edit\': vm.templates.edit  &amp;&amp; vm.regim === \'edit\',                   \'component-preview\': vm.templates.preview &amp;&amp; vm.regim === \'preview\'}" class="component-template"></div>\n' +
+    '    <div ng-if="(!vm.templates.edit &amp;&amp; vm.regim === \'edit\') || (!vm.templates.filter &amp;&amp; vm.regim === \'filter\')" ng-class="{\'component-filter\': vm.regim === \'filter\'}" class="component-edit"> \n' +
     '        <label ng-if="!vm.options.filter &amp;&amp; !!vm.label" class="field-name-label">\n' +
     '            <div data-ng-if="!!vm.hint" class="field-hint">\n' +
     '                <div ng-bind="::vm.hint" class="hint-text"></div>\n' +
--- src/module/template/layouts/layoutComponent.tpl.js
+++ src/module/template/layouts/layoutComponent.tpl.js
@@ -8,8 +8,8 @@ module.run(['$templateCache', function($templateCache) {
   $templateCache.put('module/template/layouts/layoutComponent.html',
     '\n' +
     '<div ng-class="{\'field-wrapper row\':!vm.options.filter, \'filter-wrapper-field\': vm.options.filter}">\n' +
-    '    <div on-render-template="on-render-template" ng-class="{\'component-filter\': vm.options.filter,                   \'component-edit\': ((vm.templates.edit &amp;&amp; !vm.options.filter) || (vm.templates.filter &amp;&amp; vm.options.filter)) &amp;&amp; vm.regim === \'edit\',                   \'component-preview\': vm.templates.preview &amp;&amp; vm.regim === \'preview\'}" class="component-template"></div>\n' +
-    '    <div ng-if="((!vm.templates.edit &amp;&amp; !vm.options.filter) || (!vm.templates.filter &amp;&amp; vm.options.filter)) &amp;&amp; vm.regim === \'edit\'" ng-class="{\'component-filter\': vm.options.filter}" class="component-edit"> \n' +
+    '    <div on-render-template="on-render-template" ng-class="{\'component-filter\': vm.templates.filter &amp;&amp; vm.regim === \'filter\',                   \'component-edit\': vm.templates.edit  &amp;&amp; vm.regim === \'edit\',                   \'component-preview\': vm.templates.preview &amp;&amp; vm.regim === \'preview\'}" class="component-template"></div>\n' +
+    '    <div ng-if="(!vm.templates.edit &amp;&amp; vm.regim === \'edit\') || (!vm.templates.filter &amp;&amp; vm.regim === \'filter\')" ng-class="{\'component-filter\': vm.regim === \'filter\'}" class="component-edit"> \n' +
     '    </div>\n' +
     '    <div ng-if="!vm.templates.preview &amp;&amp; vm.regim === \'preview\'" class="component-preview"> \n' +
     '        <div data-ng-show="vm.loadingData" class="loader-search-wrapper">\n' +
