a21e0a8c882f0a57f759a3d4fcbfb3b92076e73c
alexei.sorokin <aleksey.sorokin1106@yandex.ru>
2017-01-17 12:51:29 +0400

    fix including custom components

--- src/module/universal-editor.js
+++ src/module/universal-editor.js
@@ -2,24 +2,23 @@
  * Constructor for create editor
  * @param {string} id - Id html element instead of which will be the editor
  * @param {Object} config - Configuration object
- * @param {Function} callbackInject - Function for extend application
  * @constructor
  */
-var UniversalEditor = function(id, config, callbackInject) {
+var UniversalEditor = function(id, config) {
         'use strict';
-        var moduleName = 'unEditor-' + id;
-        var unEditor = $('#' + id);
-        var app;
+        var moduleName = 'unEditor-' + id,
+            unEditor = $('#' + id),
+            app, modules = ['universal.editor'];
+        if(config.modules && angular.isArray(config.modules)) {
+            modules = modules.concat(config.modules);
+        }
+        
         if (unEditor[0]) {
-            app = angular.module(moduleName, ['universal.editor']).config(['$stateProvider', '$urlRouterProvider', '$injector', 'configDataProvider', function($stateProvider, $urlRouterProvider, $injector, configDataProvider) {
+            app = angular.module(moduleName, modules).config(['$stateProvider', '$urlRouterProvider', '$injector', 'configDataProvider', function($stateProvider, $urlRouterProvider, $injector, configDataProvider) {
                 configDataProvider.setConfig(id, config);
             }]);
-            if (angular.isFunction(callbackInject)) {
-                callbackInject(app);
-            }
             unEditor.append('<div class="u-editors" data-ui-view="' + id + '"></div>');
             angular.bootstrap(unEditor[0], [moduleName]);
-
         }
         return {
             module: app,
