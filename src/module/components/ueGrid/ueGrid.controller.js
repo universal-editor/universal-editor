b9b9eb953a3317ceb9373cce86c8fc0665cfac01
AlekseySorokin <aleksey.sorokin1106@yandex.ru>
2017-07-04 17:16:14 +0400

    Disabled view for ue-textarea. (#170)
    * Parameter whiteSpace.
    
    * Updated documentation
    
    * Build.
    
    * Default behaviour for ue-textarea.
    
    * Building.
    
    * Merge with develop.
    
    * Trim text
    
    * Build

--- src/module/components/ueGrid/ueGrid.controller.js
+++ src/module/components/ueGrid/ueGrid.controller.js
@@ -544,7 +544,7 @@
                     vm.loaded = false;
                 }
                 vm.data = data[itemsKey];
-                if (vm.items) {
+                if (vm.data) {
                     var components = vm.tableFields.map(function(f) { return f.component; });
                     var options = {
                         data: vm.data,
@@ -569,6 +569,8 @@
                     angular.forEach(vm.listFooterBar, function(control) {
                         control.paginationData = data;
                     });
+                } else {
+                    switchLoaderOff();
                 }
             }
         }
@@ -656,7 +658,6 @@
             vm.contextLinks = angular.copy(vm.contextLinksOrigin);
             var result = [];
             var firstVisibleElem = false;
-
             angular.forEach(vm.contextLinks, function(value) {
                 var showContextMenuItem = value.component.settings.useable && angular.isFunction(value.component.settings.useable) ? value.component.settings.useable(obj) : true;
                 if (showContextMenuItem) {
